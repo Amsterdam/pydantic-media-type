b7cc7b0b13562f11d3991763ad3d5a9eac1192c2
Youri Westerman <y.westerman@amsterdam.nl>
2025-09-24 13:42:26 +0200

    Task/replace isort black with ruff (#45)
    * Remove dev dependencies
    
    * Replace build system
    
    * Use src folder layout
    
    * Re-install dev dependencies
    
    * Configure ruff
    
    * Fix some typing/styling issues
    
    * Fix more styling issues
    
    * Define __slots__
    See also: https://docs.astral.sh/ruff/rules/no-slots-in-str-subclass/
    
    * Ignore linting issues about the `Any` type and the argument not being used, pydantic still requires them
    
    * Replace isort and black in workflow

--- src/pydantic_media_type/__init__.py
+++ src/pydantic_media_type/__init__.py
@@ -1,36 +1,38 @@
 from mimetypes import MimeTypes
-from typing import Any
+from typing import Any, ClassVar
 
 from pydantic import GetCoreSchemaHandler
 from pydantic_core import CoreSchema, PydanticCustomError, core_schema
 
 
 class MediaType(str):
-    types: list[str] = []
+    __slots__ = ()
+    types: ClassVar[list[str]] = []
 
     @classmethod
     def _init_types(cls) -> None:
         if len(cls.types) == 0:
             mime_types = MimeTypes()
             for types_map in mime_types.types_map_inv:
-                for mime_type in types_map.keys():
+                for mime_type in types_map:
                     if mime_type not in cls.types:
                         cls.types.append(mime_type)
 
     @classmethod
-    def _validate(cls, value: str, source_type: Any) -> str:
+    def _validate(cls, value: str, source_type: Any) -> str:  # noqa: ANN401, ARG003
         cls._init_types()
 
         if value not in cls.types:
+            msg = "media_type_unsupported"
             raise PydanticCustomError(
-                "media_type_unsupported", "Provided media type is not valid"
+                msg, "Provided media type is not valid",
             )
 
         return value
 
     @classmethod
     def __get_pydantic_core_schema__(
-        cls, source_type: Any, handler: GetCoreSchemaHandler
+        cls, source_type: Any, handler: GetCoreSchemaHandler,  # noqa: ANN401
     ) -> CoreSchema:
         return core_schema.with_info_before_validator_function(
             cls._validate,
